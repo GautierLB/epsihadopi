0a964ed95590a93e49fdf13be0fc7197b8119964
gautier.lb@gmail.com <gautier.lb@gmail.com@bc1950ae-aaf5-397a-e2d9-e11dab04e7a7>
2014-11-10 10:07:45 +0000

    ** Gautier LE BISSONNAIS ** -> Création du singleton dans la classe configuration -> modification du .h accordément.

--- trunk/epsihadopi/peer2peer/Configuration.h
+++ trunk/epsihadopi/peer2peer/Configuration.h
@@ -1,9 +1,18 @@
 #pragma once
 class Configuration
 {
-public:
-	Configuration();
-	~Configuration();
+	public:
+		Configuration();
+		~Configuration();
+		Configuration* getConfig();
+		static unsigned short nbConnectionServeur; // nombre de client maximum
+		static unsigned short timeOutScan;			// temps de recherche de client maximum 
+		static unsigned short nbEnvoieSimultane;	// envoie max de bloc en simultané
+		static unsigned short nbReceptionSimultane;//réception ~~
+		static  string logSimplifierName;
+		static  string logDebugName;
+	private:
+		private Configuration config;
 
 };
 
