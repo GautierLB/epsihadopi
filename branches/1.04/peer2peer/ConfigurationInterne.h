d2a426c2e487be725521579cf4d531092605bdc4
gautier.lb@gmail.com <gautier.lb@gmail.com@bc1950ae-aaf5-397a-e2d9-e11dab04e7a7>
2014-12-03 16:48:22 +0000

    ** Gautier LE BISSONNAIS ** -> Semaphore? -> Log.h & .cpp goddamnit

--- branches/1.04/peer2peer/ConfigurationInterne.h
+++ branches/1.04/peer2peer/ConfigurationInterne.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include "Fichier.h"
 #include "Block.h"
+#include <pthread.h> 
+#include <semaphore.h> 
 
 
 using namespace std;
@@ -14,11 +16,13 @@ class ConfigurationInterne
 public:
 	~ConfigurationInterne();
 	static ConfigurationInterne& getInstance();
+	void ConfigurationInterne::addServeur();
 	vector<Fichier *> ListeFichier;
 	vector<Block *>  ListeBlock;
 
 private:
 	ConfigurationInterne();
+	sem_t mutex;
 	static ConfigurationInterne m_instance;
 	static ConfigurationInterne configInterne;
 };
