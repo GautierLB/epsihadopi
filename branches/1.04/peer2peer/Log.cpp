c6183ca07b835a7e43ab62d7ccd58b1849650428
quentincurtet@gmail.com <quentincurtet@gmail.com@bc1950ae-aaf5-397a-e2d9-e11dab04e7a7>
2014-12-10 10:50:23 +0000

    Ajout du fichier de log complexe et ajout des méthodes dans les fichiers Log.cpp et Log.h

--- branches/1.04/peer2peer/Log.cpp
+++ branches/1.04/peer2peer/Log.cpp
@@ -53,5 +53,39 @@ string LOG::ecrire(const string &s)
 	return "";
 }
 
+string LOG::ecrire_complexe(const string &s_complexe)
+{
+	// Ouvre le fichier de données :
+	fstream f("log_complexe.txt",ios_base::in | ios_base::out | ios::ate);
+	if (f.is_open())
+	{
+		CDateTime now;
+		now.Now();
+		// Écrit les données :
+		string chaine;
+		f << now.m_wYear << "-" << now.m_wMonth << "-" << now.m_wDay << " : " << now.m_wHour << "-" << now.m_wMinute << "-" << now.m_wSecond << "-" << now.m_wMilliseconds << " Infos : " << s << endl;
+		// Lit les données :
+		f >> chaine ;
+		_int64 lines = std::count( 
+			std::istreambuf_iterator<char>( f ), 
+			std::istreambuf_iterator<char>(), '\n' );
+		std::cout << lines << std::endl;
+		std::cout << s << std::endl;
+		// Ferme le fichier :
+		f.close();
+	}
+
+	std::ifstream File("log_complexe.txt"); 
+  
+    std::cout << CountLines(File) << std::endl; 
+  
+    File.clear(); 
+    File.seekg(0, std::ios::beg); 
+  
+    std::cout << CountLines(File) << std::endl; 
+	
+	return "";
+}
+
 
 LOG::~LOG(){}; //Instanciation du destructeur
